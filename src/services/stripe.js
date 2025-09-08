e6e0ac1fea7b4387324277f6f2ab9e4ec9705cb0
c-silber <csilber@ramapo.edu>
2025-09-08 19:18:19 -0400

    fix stripe

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -1,3 +1,3 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);

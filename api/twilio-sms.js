6dc1a705bd7e4e23445040820629ef9a9f052558
c-silber <csilber@ramapo.edu>
2025-09-11 16:19:06 -0400

    fixing webhook

--- api/twilio-sms.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  console.log('SMS received from:', req.body.From);
-  console.log('Message:', req.body.Body);
-  
-  res.status(200);
-  res.setHeader('Content-Type', 'text/xml');
-  res.end('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
-}
